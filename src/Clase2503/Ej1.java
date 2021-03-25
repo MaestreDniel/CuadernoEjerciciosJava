547b6c58c42e40da65a7cdee60754b0d36591fc2
Daniel Maestre <dmaestre@cifpfbmoll.eu>
2021-03-25 18:27:24 +0100

    Ejercicios realizados

---
+++ src/Clase2503/Ej1.java
@@ -0,0 +1,18 @@
+package Clase2503;
+
+public class Ej1 {
+    public static void main(String[] args){
+        int num1 = 3;
+        int num2 = 4;
+        int suma = num1 + num2;
+        int resta = num1 - num2;
+        int multiplicacion = num1 * num2;
+        float division = (float) num1 / (float) num2;
+        int modulo = num1 % num2;
+        System.out.println("La suma de " + num1 + " y " + num2 + " es = " + suma);
+        System.out.println("La resta de " + num1 + " y " + num2 + " es = " + resta);
+        System.out.println("La multiplicación de " + num1 + " y " + num2 + " es = " + multiplicacion);
+        System.out.println("La division de " + num1 + " y " + num2 + " es = " + division);
+        System.out.println("El módulo de " + num1 + " y " + num2 + " es = " + modulo);
+    }
+}
---
+++ src/Clase2503/Ej3.java
@@ -0,0 +1,12 @@
+package Clase2503;
+
+import java.util.Scanner;
+
+public class Ej3 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Introduce tu nombre: ");
+        String nombre = entrada.next();
+        System.out.println("Bienvenido " + nombre);
+    }
+}
